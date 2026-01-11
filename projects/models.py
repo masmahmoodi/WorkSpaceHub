cdfec2e9e2f6ab9595f1f181ebe5fd4733882046
Masoud Mahmoodi <mrm7g8@gmail.com>
2026-01-10 21:49:51 -0800

    add project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="projects",null=False)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+

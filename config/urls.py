6549d5ff4ca11e89eed21dea47c92429c635af6a
Masoud Mahmoodi <mrm7g8@gmail.com>
2026-01-12 23:19:36 -0800

    add serializer, view, and urls for Project model

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,7 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth", include("accounts.urls"))
+    path("api/auth/", include("accounts.urls")),
+    path("api/",include("projects.urls")),
+
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .serializers import ProjectSerializer
+from rest_framework.generics import ListCreateAPIView
+from .models import Project
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ProjectListCreateView(ListCreateAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(owner=self.request.user)
+
+    def perform_create(self,serializer):
+        return serializer.save(owner=self.request.user)
